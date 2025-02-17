dcd7c2e930b6b35c3ff61deff36e9d56e3e72dab
ADX0590\ADX0590 <k.tsukuda@and-ex.co.jp>
2025-02-17 13:47:41 +0900

    no message

---
+++ js/config.js
@@ -0,0 +1,19 @@
+window._config = {
+
+    cognito: {
+
+        userPoolId: 'ap-northeast-1_2fX28xzaC', // e.g. us-east-2_uXboG5pAb         
+
+        userPoolClientId: '6tej5mimr1l5rjuobdm9sdsko9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+
+        region: 'ap-northeast-1' // e.g. us-east-2 
+
+    }, 
+
+    api: { 
+
+        invokeUrl: 'https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod, 
+
+    } 
+
+};
